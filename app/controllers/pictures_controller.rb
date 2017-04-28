0f0caa30cd6a301d36870a88498a84463156f4d4
Kosuke Koshijima <kosuke.koshijima@gmail.com>
2017-04-28 10:10:00 -0500

    4/28-2

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,6 +12,14 @@ class PicturesController < ApplicationController
   end
 
   def show
+    id_number = params["the_id"]
+
+    p = Photo.find(id_number)
+
+    @the_caption = p.caption
+    @the_source = p.source
+    @the_timestamp = p.created_at
+
     render("pic_templates/show.html.erb")
   end
 
