8a55aa3e47badb62d1af512e49d7650370435ba3
Kosuke Koshijima <kosuke.koshijima@gmail.com>
2017-05-04 01:10:34 -0500

    1

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,32 +1,62 @@
 class PicturesController < ApplicationController
+
   def new_form
+
     render("pic_templates/new_form.html.erb")
+
   end
 
   def create_row
+
+    @my_photo = Photo.new
+    @my_photo.caption = params["the_caption"]
+    @my_photo.source = params["the_source"]
+    @my_photo.save
+
+    @current_count = Photo.count
+
     render("pic_templates/create_row.html.erb")
   end
 
   def index
-    @list_of_photos = Photo.all
+
+    @list_of_photos = Photo.all.order(:created_at => :desc)
+
     render("pic_templates/index.html.erb")
   end
 
   def show
-      @my_photo = Photo.find(params["the_id"])
+    # The parameters look like: {"the_id"=>"4"}
+    @my_photo = Photo.find(params["the_id"])
 
     render("pic_templates/show.html.erb")
   end
 
   def edit_form
+
+    @my_photo = Photo.find(params["la_id"])
+
     render("pic_templates/edit_form.html.erb")
   end
 
   def update_row
+
+    @my_photo = Photo.find(params["la_id"])
+    @my_photo.caption = params["the_caption"]
+    @my_photo.source = params["the_source"]
+    @my_photo.save
+
     render("pic_templates/update_row.html.erb")
   end
 
   def destroy_row
+
+    @my_photo = Photo.find(params["da_id"])
+    @my_photo.destroy
+
+    @current_count = Photo.count
+
     render("pic_templates/destroy_row.html.erb")
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428140222) do
+ActiveRecord::Schema.define(version: 20170504053513) do
 
   create_table "photos", force: :cascade do |t|
-    t.string   "source"
     t.text     "caption"
+    t.string   "source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
